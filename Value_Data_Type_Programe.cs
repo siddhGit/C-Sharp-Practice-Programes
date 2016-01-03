2f31e315d118f65d224aaa57a46f2dff39602a7d
siddhGit <msiddharth42@gmail.com>
2016-01-03 13:03:59 +0530

    C# Programes

---
+++ Value_Data_Type_Programe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Size of int {0}", sizeof(int));
+            Console.ReadKey();
+        }
+    }
+}
